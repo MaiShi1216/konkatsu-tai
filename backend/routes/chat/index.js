e9890356e0bb68001328b68e4075f23029537277
Hayami Masahiro <masahiro.hayami@woven-planet.global>
2022-09-16 14:38:38 +0900

    chat star 06

--- backend/routes/chat/index.js
+++ backend/routes/chat/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 
 /* Import DB */
 const usersInfo = require('../../userInfo.json')
+const chatHistory = require('../../chatHistory.json')
 
 /* Successfully inquiry of authentication */
 router.get('/', (req, res) => {
@@ -11,8 +12,10 @@ router.get('/', (req, res) => {
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   const body = usersInfo.a001
+  const chatBody = chatHistory.chats
 
   setTimeout(() => res.send(body), 500)
+  setTimeout(() => res.send(chatBody), 500)
 })
 
 module.exports = router
