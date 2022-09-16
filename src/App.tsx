c780d15268b1435a12da662d81b0a132e021fbf4
Hayami Masahiro <masahiro.hayami@woven-planet.global>
2022-09-16 14:18:05 +0900

    chat start 05

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { paths } from '@/utils/paths'
 
 export const App = () => {
-  //return (
-  //  <BrowserRouter>
-  //    <Routes>
-  //      <Route path={paths.sample} element={<Sample />} />
-  //    </Routes>
-  //  </BrowserRouter>
-  //)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={paths.sample} element={<Sample />} />
+      </Routes>
+    </BrowserRouter>
+    );
   return (
     <BrowserRouter>
       <Routes>
         <Route path={paths.chat} element={<Chat />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
