286391cdc17471d51f3497adc8fc35e2907c8382
masayuki <masayuki.inoue@woven-planet.global>
2022-09-16 16:56:24 +0900

    Create ProfileCard component

--- src/components/home/ProfileCard.tsx
+++ src/components/home/ProfileCard.tsx
@@ -1,6 +1,23 @@
 import React from 'react'
 import classes from '@/components/home/Home.css'
 
+type ResJson = {
+  name: string
+}
+
 export const ProfileCard = () => {
-  return <div className={classes.card}>content</div>
+  const [profile, setProfile] = React.useState('hello')
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${process.env.API_ENDPOINT}/sample`, {
+        method: 'GET',
+      })
+      const resJson: ResJson = await response.json()
+      return resJson
+    }
+    fetchData().then(resJson => setProfile(resJson.name)).catch(console.error)
+  }, [profile])
+
+  return <div className={classes.card}>{profile}</div>
 }
