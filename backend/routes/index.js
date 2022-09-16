c021b95de8b5cb02e09fd11e14714215ed7c0fed
Mai <mai.shimizu@woven-planet.global>
2022-09-16 14:13:30 +0900

    first backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const router = require('express').Router()
 const sampleRouter = require('./sample')
+const signinRouter = require('./signin')
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 router.use('/sample', sampleRouter)
-
+router.use('/signin', signinRouter)
 module.exports = router
