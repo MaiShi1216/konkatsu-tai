d597a970ca6dfb4ddf3749395d975eb14a54c73b
kan-mayoshi <kan.mayoshi@woven-planet.global>
2022-09-19 19:28:27 +0900

    Add signup API in backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const router = require('express').Router()
 const sampleRouter = require('./sample')
+const signupRouter = require('./signup')
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 router.use('/sample', sampleRouter)
+router.use('/signup', signupRouter)
 
 module.exports = router
