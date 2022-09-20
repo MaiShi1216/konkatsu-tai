de85c0467534ce3524484ffff178e04065f437a7
kan-mayoshi <kan.mayoshi@woven-planet.global>
2022-09-20 13:39:21 +0900

    Implement recoil for global state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 import { paths } from '@/utils/paths'
 
 import { Sample } from '@/components/sample/Sample'
@@ -7,11 +8,13 @@ import { Signup } from '@/components/signup/SignUp'
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={paths.sample} element={<Sample />} />
-        <Route path={paths.signup} element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path={paths.sample} element={<Sample />} />
+          <Route path={paths.signup} element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   )
 }
