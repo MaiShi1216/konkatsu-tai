778e5de27fc730c0c1cadd48a13fdae1b2dcdc05
takashi.hayashi <takashi.hayashi@woven-planet.global>
2022-09-20 12:32:40 +0900

    Prettierでの修正

--- backend/routes/matched/index.js
+++ backend/routes/matched/index.js
@@ -2,36 +2,44 @@
 const router = require('express').Router()
 
 /* Import DB */
-const usersInfo = require('../../userInfo.json');
-const likeHistory = require('../../likeHistory.json');
+const usersInfo = require('../../userInfo.json')
+const likeHistory = require('../../likeHistory.json')
 
 /* Successfully inquiry of authentication */
 router.get('/', (req, res) => {
-  const userId = req.query.userId;//userIdはクエリパラメータで取得
-  console.log(userId);
+  const userId = req.query.userId //userIdはクエリパラメータで取得
+  console.log(userId)
   res.status(200)
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 
-  const userLikes = likeHistory[userId];
-  const matchedUserId=[];
-  const matchedUserInfo={};
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  const userLikes = likeHistory[userId]
+  const matchedUserId = []
+  const matchedUserInfo = {}
 
-  for(let i = 0; i < userLikes.length; i++){
-    temp = likeHistory[userLikes[i]];
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  for (let i = 0; i < userLikes.length; i++) {
+    // eslint-disable-next-line no-undef, @typescript-eslint/no-unsafe-member-access
+    temp = likeHistory[userLikes[i]]
     //console.log(temp);
-    for(let k in temp){
-      if(temp[k]===userId){
+    // eslint-disable-next-line no-undef
+    for (let k in temp) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, no-undef
+      if (temp[k] === userId) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         console.log(userLikes[i])
-        matchedUserId.push(userLikes[i]);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        matchedUserId.push(userLikes[i])
       }
     }
   }
 
-  for(let i = 0; i < matchedUserId.length; i++){
-    matchedUserInfo[matchedUserId[i]] = usersInfo[matchedUserId[i]];
+  for (let i = 0; i < matchedUserId.length; i++) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    matchedUserInfo[matchedUserId[i]] = usersInfo[matchedUserId[i]]
   }
-  console.log(matchedUserInfo);
+  console.log(matchedUserInfo)
 
   //setTimeout(() => res.send(body), 500)
   setTimeout(() => res.send(matchedUserInfo), 500)
--- src/components/matched/Matched.tsx
+++ src/components/matched/Matched.tsx
@@ -1,41 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Header } from '@/components/header/Header'
 import { Footer } from '@/components/footer/Footer'
 import classes from '@/components/matched/style.css'
 
 type ResJson = {
-  name: string,
-  password : string,
-  nickname : string,
-  photo : string,
-  sex :string,
-  birthday : Date,
-  email : string,
-  favoriteTypes : string,
-  hobbies : string,
-  likedNum : number,
-  selfIntro : string,
-  isHidden : any
+  name: string
+  password: string
+  nickname: string
+  photo: string
+  sex: string
+  birthday: Date
+  email: string
+  favoriteTypes: string
+  hobbies: string
+  likedNum: number
+  selfIntro: string
+  isHidden: any
 }
 
 export const Matched = () => {
-  //const [message, setMessage] = useState(undefined)
+  //const [message, setMessage] = useState({})
   const [message, setMessage] = useState({})
 
   const fetchSample = async (): Promise<void> => {
     //userIdは手打ち。最終的にはグローバル変数で受け取る？
-    const myId = "a001";
+    const myId = 'a002'
     //userIdをクエリパラメータに設定
     const query_params = new URLSearchParams({
-      userId: myId
-    });
-    const response = await fetch(`${process.env.API_ENDPOINT}/matched?`+query_params, {
-      method: 'GET'
+      userId: myId,
+    })
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    const response = await fetch(`${process.env.API_ENDPOINT}/matched?${query_params}`, {
+      method: 'GET',
     })
     const resJson: ResJson = await response.json()
     //setMessage(resJson.name)
-    setMessage(resJson);
-    console.log(resJson);
+    setMessage(resJson)
+    console.log(resJson)
   }
 
   return (
@@ -44,7 +45,7 @@ export const Matched = () => {
       <p>This is a Matched component.</p>
       <button onClick={fetchSample}>See matched members!</button>
       <div>
-        {Object.keys(message).map(i => (
+        {Object.keys(message).map((i) => (
           <div key={i} className={classes.container}>
             <img src={message[i].photo} className={classes.photo}></img>
             <h3 className={classes.name}>{message[i].name}</h3>
