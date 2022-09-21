d9546c6d9da7aa58efa747b0df5f216d52eee050
takashi.hayashi <takashi.hayashi@woven-planet.global>
2022-09-21 11:50:31 +0900

    エラーあり

--- src/components/matched/Matched.tsx
+++ src/components/matched/Matched.tsx
@@ -26,7 +26,7 @@ type ResJson = {
 }
 
 export const Matched = () => {
-  const [matchedUsers, setMatchedUsers] = useState<ResJson[]>([])
+  const [matchedUsers, setMatchedUsers] = useState<object>([])
 
   const fetchMatched = async (): Promise<void> => {
     //userIdは手打ち。最終的にはグローバル変数で受け取る？
@@ -39,7 +39,7 @@ export const Matched = () => {
     const response = await fetch(`${process.env.API_ENDPOINT}/matched?${query_params}`, {
       method: 'GET',
     })
-    const resJson: ResJson[] = await response.json()
+    const resJson: object = await response.json()
     setMatchedUsers(resJson)
     console.log(resJson)
   }
@@ -48,18 +48,34 @@ export const Matched = () => {
     const run = fetchMatched()
   }, [])
 
+  //オブジェクト表示用の関数
+  const result = mapObj()=> {
+    Object.keys(matchedUsers).map((i) => (
+    const matchedUser: ResJson = matchedUsers[i]
+        return(
+        < div key = { i } className = { classes.container } >
+      <img src={matchedUser.photo} className={classes.photo}></img>
+      <h3 className={classes.name}>{mmatchedUser.name}</h3>
+      <p className={classes.message}>{matchedUser.selfIntro}</p>
+          </div>
+        )
+  ))
+}
+
   return (
     <>
       <Header />
       <p>This is a Matched component.</p>
       <div>
-        {Object.keys(matchedUsers).map((i) => (
+        {result}
+        {/* {Object.keys(matchedUsers).map((i) => (
+          //const matchedUser:ResJson=matchedUsers[i]
           <div key={i} className={classes.container}>
             <img src={matchedUsers[i].photo} className={classes.photo}></img>
             <h3 className={classes.name}>{matchedUsers[i].name}</h3>
             <p className={classes.message}>{matchedUsers[i].selfIntro}</p>
           </div>
-        ))}
+        ))} */}
       </div>
       <Footer />
     </>
