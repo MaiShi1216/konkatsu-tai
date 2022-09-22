2973568b7c748ad97baa0f8ee320f945b2bb43cb
takashi.hayashi <takashi.hayashi@woven-planet.global>
2022-09-22 14:16:52 +0900

    Add route for recommended

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,14 @@
 const router = require('express').Router()
 const sampleRouter = require('./sample')
 const matchedRouter = require('./matched')
+const recommendedRouter = require('./recommended')
 const userRouter = require('./user')
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 router.use('/sample', sampleRouter)
 router.use('/matched', matchedRouter)
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+router.use('/recommended', recommendedRouter)
 router.use('/user', userRouter)
 
 module.exports = router
