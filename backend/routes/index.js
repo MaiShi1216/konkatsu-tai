b49ceec6f57fb8ed73987b3238bdc30fd4c3c9e1
masayuki <masayuki.inoue@woven-planet.global>
2022-09-22 10:03:23 +0900

    Add profile root

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,11 +4,13 @@ const sampleRouter = require('./sample')
 const matchedRouter = require('./matched')
 const homeRouter = require('./home')
 const userRouter = require('./user')
+const profileRouter = require('./profile')
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 router.use('/sample', sampleRouter)
 router.use('/matched', matchedRouter)
 router.use('/home', homeRouter)
 router.use('/user', userRouter)
+router.use('/profile', profileRouter)
 
 module.exports = router
