314d8e809b490c8d57b431444c5605f97293f2d6
kan-mayoshi <kan.mayoshi@woven-planet.global>
2022-09-23 14:50:49 +0900

    Delete unnecessary conditional branch

--- src/components/signup/Signup.tsx
+++ src/components/signup/Signup.tsx
@@ -40,8 +40,8 @@ export const Signup: FC<PropsType> = (props) => {
     const b64img = await encodeImgToBase64()
 
     const newUserInfo: UserInfoContentType = {
-      name: props.mode === 'create' ? name : undefined,
-      email: props.mode === 'create' ? email : undefined,
+      name,
+      email,
       password,
       nickname,
       hobbies,
