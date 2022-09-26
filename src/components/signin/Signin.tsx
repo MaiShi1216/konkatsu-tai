66e76613243d5a8c66c8e2de2dfa121ae54d0a9a
Mai <mai.shimizu@woven-planet.global>
2022-09-26 13:25:03 +0900

    Co-authored-by: kan-mayoshi <kan-mayoshi@users.noreply.github.com>

--- src/components/signin/Signin.tsx
+++ src/components/signin/Signin.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import classes from '@/components/signin/style.css'
 import { Header } from '@/components/header/Header'
 import { Footer } from '@/components/footer/Footer'
+import { useRecoilState } from 'recoil'
+import { userInfoState } from '@/atoms/userInfoAtom'
+import { response } from 'express'
 
 type ResJson = {
   name: string
@@ -9,47 +12,94 @@ type ResJson = {
 
 export const Signin = () => {
   const [mail, setMail] = useState<string>(undefined)
+  const [password, setPassword] = useState<string>(undefined)
+  // const [userInfo, setUserInfo] = useRecoilState(userInfoState)
   const [isShown, setIsShown] = useState(false)
   const togglePassword = () => {
     setIsShown((isShown) => !isShown)
   }
 
-  const fetchSample = async (): Promise<void> => {
-    const response = await fetch(`${process.env.API_ENDPOINT}/signin`, {
+  // const fetchSample = async (): Promise<void> => {
+  //   const response = await fetch(`${process.env.API_ENDPOINT}/signin`, {
+  //     method: 'POST',
+  //   })
+  //   const resJson: ResJson = await response.json()
+  //   setMail(resJson.name)
+  // }
+
+  // try {
+  //   const response = await fetch(`${process.env.API_ENDPOINT}/signin`, {
+  //     method: 'POST',
+  //     headers: { 'content-Type': 'application/json' },
+  //     body: JSON.stringify(newUserInfo),
+  //   })
+  //   if (response.status === 200) {
+  //     privateInfos.forEach((key) => delete newUserInfo[key])
+  //     const resJson: ResJson = await response.json()
+
+  //     const storedInfo = { [resJson.userId]: newUserInfo }
+  //     setUserInfo(storedInfo)
+  //     window.location.href = '/'
+  //   } else {
+  //     console.error('err')
+  //   }
+  // } catch (err) {
+  //   console.error(err)
+  // }
+  // }
+
+  const handleClick = () => {
+    const signinData = { mail: mail, password: password }
+    const url = 'http://localhost:8081/signin'
+    const opt = {
       method: 'POST',
-    })
-    const resJson: ResJson = await response.json()
-    setMail(resJson.name)
+      headers: { 'content-Type': 'application/json' },
+      body: JSON.stringify(signinData),
+    }
+    // const response = fetch(url, opt) // この行は一旦コメントアウト
+    const response = { // 一旦はbackendから正常応答がきたと仮置き
+      status: 200,
+      "3f328652-f4bb-4254-972a-d70489794a25": {
+        photo: ***,
+        name: ***,
+
+      }
+
+      // これ以降にresponseに対する処理を記載
+      // recoilにデータを保存して、画面遷移する
+    }
   }
+  // if (response.status === 200) {
+  //   window.location.href = '/'
+  // } else {
+  //   console.error('err')
+  // }
+  // } catch (err) {
+  //   console.error(err)
+  // }
 
   return (
     <div className={classes.container}>
       <Header />
       <h3>eMail Adress</h3>
       <input
+        type="text"
         placeholder="Enter your email adress"
         onChange={(e) => {
           setMail(e.target.value)
         }}
       />
-      <p>{mail}</p>
       <h3>Password</h3>
-      <form className="form" onSubmit={(e) => e.preventDefault()}>
-        <input type={isShown ? 'text' : 'password'} placeholder="Enter your Password" className="input" />
-        <div className="checkbox-container">
-          <label htmlFor="checkbox">Show Password?</label>
-          <input id="checkbox" type="checkbox" checked={isShown} onChange={togglePassword} />
-        </div>
-      </form>
-
-      <button className={classes.submitButton} onClick={fetchSample}>
-        Sign In
+      <input
+        type="password"
+        placeholder="Enter your password"
+        onChange={(e) => {
+          setPassword(e.target.value)
+        }}
+      />
+      <button className={classes.submitButton} onClick={handleClick}>
+        Signin
       </button>
-
-      <button className={classes.submitButton} onClick={fetchSample}>
-        Sign Up
-      </button>
-
       <Footer />
     </div>
   )
