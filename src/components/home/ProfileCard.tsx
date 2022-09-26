6dbe25d45c6c14c46a4e17c7318b2852062c06cf
masayuki <masayuki.inoue@woven-planet.global>
2022-09-26 23:16:47 +0900

    Add blur style

--- src/components/home/ProfileCard.tsx
+++ src/components/home/ProfileCard.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import classes from '@/components/home/style.css'
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt'
 import { UserInfoContentType } from '@/utils/types'
+import { useRecoilValue } from 'recoil'
+import { userInfoState } from '@/atoms/userInfoAtom'
 
 type Props = {
   User: UserInfoContentType
@@ -10,11 +12,29 @@ type Props = {
 }
 
 export const ProfileCard = (props: Props) => {
+  const userInfo = useRecoilValue(userInfoState)
+  const myId = Object.keys(userInfo)[0]
+  const isSecretMode = userInfo[myId].isSecretMode
+
+  const fetchFamiliarity = () => {
+    // const params = { loginUser: myId, other: props.Id }
+    // const query = new URLSearchParams(params)
+    // const response = await fetch(`${process.env.API_ENDPOINT}/home`)
+    return 5
+  }
+  const familiarity = fetchFamiliarity()
+
   return (
     <React.Fragment>
       <div className={classes.card}>
         <div className={classes.image_frame}>
-          <img src={props.User.photo} alt="" onClick={(event) => props.transferToProfile(event)} id={props.Id} />
+          <img
+            src={props.User.photo}
+            alt=""
+            onClick={(event) => props.transferToProfile(event)}
+            id={props.Id}
+            style={isSecretMode ? { filter: `blur(${familiarity}px)` } : null}
+          />
         </div>
         <div className={classes.name}>{props.User.nickname}</div>
         <div className={classes.like}>
