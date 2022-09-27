01d1aa737a80e0e78023079a5a562a1f26464a06
MaiShi1216 <111329266+MaiShi1216@users.noreply.github.com>
2022-09-27 16:11:57 +0900

    Signin画面のUIを作成完了しました (#16)
    * first branch
    
    * first backend
    
    * made signin UI
    
    * FE UI development
    
    * finished signin UI
    
    * change chapitarization
    
    * changed details from mayoshi san comment
    
    * Co-authored-by: kan-mayoshi <kan-mayoshi@users.noreply.github.com>
    
    * add FE API
    
    * fixed error
    
    * changed g
    from Mayoshi-san comment
    
    * changed from mayoshi san comment 2
    
    * added signup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const router = require('express').Router()
 const sampleRouter = require('./sample')
+const signinRouter = require('./signin')
 const matchedRouter = require('./matched')
 const homeRouter = require('./home')
 const userRouter = require('./user')
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 router.use('/sample', sampleRouter)
+router.use('/signin', signinRouter)
 router.use('/matched', matchedRouter)
 router.use('/home', homeRouter)
 router.use('/user', userRouter)
--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,6 +1,7 @@
 export const paths = {
   sample: '/sample',
   signup: '/signup',
+  signin: '/signin',
   home: '/',
   matched: '/matched',
   edit: '/edit',
