5468f139080b8cf9c5c190bd931dd9716802470d
masayuki654 <111120750+masayuki654@users.noreply.github.com>
2022-09-27 16:57:52 +0900

    Update backend/routes/home/index.js
    Co-authored-by: kan-mayoshi <111116632+kan-mayoshi@users.noreply.github.com>

--- backend/routes/home/index.js
+++ backend/routes/home/index.js
@@ -6,7 +6,7 @@ const usersInfo = require('../../userInfo.json')
 
 /* Successfully inquiry of authentication */
 router.get('/', (req, res) => {
-  console.log('GET /')
+  console.log('GET /home')
   res.status(200)
   res.send(usersInfo)
 })
