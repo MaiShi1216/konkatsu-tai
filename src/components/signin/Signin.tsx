e209d154d902c004c2b5f1a075fba72731573ec8
Mai <mai.shimizu@woven-planet.global>
2022-09-27 13:55:09 +0900

    changed g from Mayoshi-san comment

--- src/components/signin/Signin.tsx
+++ src/components/signin/Signin.tsx
@@ -15,41 +15,8 @@ type ResJson = {
 export const Signin = () => {
   const [mail, setMail] = useState<string>(undefined)
   const [password, setPassword] = useState<string>(undefined)
-  // const [userInfo, setUserInfo] = useRecoilState(userInfoState)
-  const [isShown, setIsShown] = useState(false)
-  const togglePassword = () => {
-    setIsShown((isShown) => !isShown)
-  }
   const [userInfo, setUserInfo] = useRecoilState<UserInfoType>(userInfoState)
   const navigate = useNavigate()
-  // const fetchSample = async (): Promise<void> => {
-  //   const response = await fetch(`${process.env.API_ENDPOINT}/signin`, {
-  //     method: 'POST',
-  //   })
-  //   const resJson: ResJson = await response.json()
-  //   setMail(resJson.name)
-  // }
-
-  // try {
-  //   const response = await fetch(`${process.env.API_ENDPOINT}/signin`, {
-  //     method: 'POST',
-  //     headers: { 'content-Type': 'application/json' },
-  //     body: JSON.stringify(newUserInfo),
-  //   })
-  //   if (response.status === 200) {
-  //     privateInfos.forEach((key) => delete newUserInfo[key])
-  //     const resJson: ResJson = await response.json()
-
-  //     const storedInfo = { [resJson.userId]: newUserInfo }
-  //     setUserInfo(storedInfo)
-  //     window.location.href = '/'
-  //   } else {
-  //     console.error('err')
-  //   }
-  // } catch (err) {
-  //   console.error(err)
-  // }
-  // }
 
   const handleClick = () => {
     const signinData = { mail: mail, password: password }
@@ -89,34 +56,38 @@ export const Signin = () => {
     } else {
       console.error('err')
     }
-    //   } catch (err) {
-    //   console.error(err)
-    // }
+    const handleClick2 = () => {}
+    return navigate('/signup')
   }
 
   return (
-    <div className={classes.container}>
-      <Header />
-      <h3>eMail Adress</h3>
-      <input
-        type="text"
-        placeholder="Enter your email adress"
-        onChange={(e) => {
-          setMail(e.target.value)
-        }}
-      />
-      <h3>Password</h3>
-      <input
-        type="password"
-        placeholder="Enter your password"
-        onChange={(e) => {
-          setPassword(e.target.value)
-        }}
-      />
-      <button className={classes.submitButton} onClick={handleClick}>
-        Signin
-      </button>
-      <Footer />
-    </div>
+    <>
+      <div className={classes.container}>
+        <Header />
+        <h3>eMail Adress</h3>
+        <input
+          type="text"
+          placeholder="Enter your email adress"
+          onChange={(e) => {
+            setMail(e.target.value)
+          }}
+        />
+        <h3>Password</h3>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+        />
+        <button className={classes.submitButton} onClick={handleClick}>
+          Signin
+        </button>
+        <button className={classes.submitButton} onClick={handleClick2}>
+          Signup
+        </button>
+        <Footer />
+      </div>
+    </>
   )
 }
