f71a108f67d639188ac25412f75917a3bdf9c57d
masayuki <masayuki.inoue@woven-planet.global>
2022-09-29 08:48:54 +0900

    Delete unnecessary line

--- src/components/recommended/Recommended.tsx
+++ src/components/recommended/Recommended.tsx
@@ -2,12 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { Header } from '@/components/header/Header'
 import { Footer } from '@/components/footer/Footer'
 import classes from '@/components/recommended/style.css'
-import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt'
 import { UserInfoType } from '@/utils/types'
-import { useNavigate } from 'react-router-dom'
 import { Main } from '@/components/recommended/Main'
-
-//Recoil-on
 import { useRecoilValue } from 'recoil'
 import { userInfoState } from '@/atoms/userInfoAtom'
 
@@ -19,14 +15,10 @@ export const Recommended = () => {
   const [recommendedUsersByHobbies, setRecommendedUsersByHobbies] = useState<UserInfoType>({})
   const [recommendedUsersByLikes, setRecommendedUsersByLikes] = useState<UserInfoType>({})
   const [commonPoints, setCommonPoints] = useState<UserInfoType>({})
-  const navigate = useNavigate()
-
-  //RecoilでユーザIDを取得
   const userInfo = useRecoilValue(userInfoState)
   const myId = Object.keys(userInfo)[0]
 
   const fetchRecommended = async (): Promise<void> => {
-    //userIdをクエリパラメータに設定
     const response = await fetch(`${process.env.API_ENDPOINT}/recommended?loginId=${myId}`, {
       method: 'GET',
     })
@@ -36,11 +28,6 @@ export const Recommended = () => {
     setCommonPoints(resJson.commonPoints)
   }
 
-  const transferToProfile = (userId) => {
-    navigate('/profile', { state: { id: userId } })
-  }
-
-  //ページ読み込み時にレンダリング
   useEffect(() => {
     fetchRecommended().catch((err) => {
       console.error(err)
