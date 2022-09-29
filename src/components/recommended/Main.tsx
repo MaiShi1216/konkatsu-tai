d85a2d9f4c1105c508826f1f90e12c502804dfc9
takashi.hayashi <takashi.hayashi@woven-planet.global>
2022-09-29 22:39:48 +0900

    Processing when there is no user to display

--- src/components/recommended/Main.tsx
+++ src/components/recommended/Main.tsx
@@ -16,33 +16,43 @@ export const Main = (props: Props) => {
     navigate('/profile', { state: { id: userId } })
   }
 
-  return (
-    <div>
-      {Object.keys(props.users).map((userId) => (
-        <div key={userId} className={classes.container}>
-          <img src={props.users[userId].photo} className={classes.photo}></img>
-          <div className={classes.name_likes}>
-            <p className={classes.name}>{props.users[userId].nickname}</p>
-            <div className={classes.likes}>
-              <div>
-                <ThumbUpAltIcon style={{ fontSize: '1rem' }} className={classes.thumbUp} />
+  if (Object.keys(props.users).length !== 0) {
+    return (
+      <div>
+        {Object.keys(props.users).map((userId) => (
+          <div key={userId} className={classes.container}>
+            <img src={props.users[userId].photo} className={classes.photo}></img>
+            <div className={classes.name_likes}>
+              <p className={classes.name}>{props.users[userId].nickname}</p>
+              <div className={classes.likes}>
+                <div>
+                  <ThumbUpAltIcon style={{ fontSize: '1rem' }} className={classes.thumbUp} />
+                </div>
+                <div className={classes.liked_num}>{props.users[userId].likedNum > 99 ? '99+' : props.users[userId].likedNum}</div>
               </div>
-              <div className={classes.liked_num}>{props.users[userId].likedNum > 99 ? '99+' : props.users[userId].likedNum}</div>
             </div>
+            <div className={classes.hobbies}>
+              <p>Similar hobbies:</p>
+              <div className={classes.hobbiesList}>
+                {Object.keys(props.hobby[userId]).map((index) => (
+                  <ul key={index} style={{ listStyleType: 'none' }}>
+                    <li className={classes.hobby}>{props.hobby[userId][index]}</li>
+                  </ul>
+                ))}
+              </div>
+            </div>
+            <button type="button" className={classes.talkButton} onClick={() => transferToProfile(userId)}>
+              View profile
+            </button>
           </div>
-          <div className={classes.hobbies}>
-            <p>Similar hobbies:</p>
-            {Object.keys(props.hobby[userId]).map((index) => (
-              <ul key={index} style={{ listStyleType: 'none' }}>
-                <li className={classes.hobby}>{props.hobby[userId][index]}</li>
-              </ul>
-            ))}
-          </div>
-          <button type="button" className={classes.talkButton} onClick={() => transferToProfile(userId)}>
-            View profile
-          </button>
-        </div>
-      ))}
-    </div>
-  )
+        ))}
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <p className={classes.noDisplay}>No user to display</p>
+      </div>
+    )
+  }
 }
