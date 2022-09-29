7f6fbc2ba121bac62652546cd7d04bc5a2d03694
masayuki <masayuki.inoue@woven-planet.global>
2022-09-29 23:51:50 +0900

    Apply blur in matching page

--- src/components/matched/Matched.tsx
+++ src/components/matched/Matched.tsx
@@ -2,34 +2,38 @@ import React, { useState, useEffect } from 'react'
 import { Header } from '@/components/header/Header'
 import { Footer } from '@/components/footer/Footer'
 import classes from '@/components/matched/style.css'
-import { UserInfoType } from '@/utils/types'
+import { UserInfoContentType, UserInfoType } from '@/utils/types'
 import { useNavigate } from 'react-router-dom'
-
-//Recoil-on
 import { useRecoilValue } from 'recoil'
 import { userInfoState } from '@/atoms/userInfoAtom'
 
+type Familiarity = {
+  familiarity?: number
+}
+
+type UserInfoTypeWithFamiliarity = {
+  [key in string]: UserInfoContentType & Familiarity
+}
+
 export const Matched = () => {
-  const [matchedUsers, setMatchedUsers] = useState<UserInfoType>({})
+  const [matchedUsers, setMatchedUsers] = useState<UserInfoTypeWithFamiliarity>({})
   const navigate = useNavigate()
   const transferToChat = (userId) => {
     navigate('/chat', { state: { partnerId: userId } })
   }
 
-  //RecoilでユーザIDを取得
   const userInfo = useRecoilValue(userInfoState)
   const myId = Object.keys(userInfo)[0]
+  const isSecretMode = userInfo[myId].isSecretMode
 
   const fetchMatched = async (): Promise<void> => {
-    //userIdをクエリパラメータに設定
     const response = await fetch(`${process.env.API_ENDPOINT}/matched?userId=${myId}`, {
       method: 'GET',
     })
-    const resJson: UserInfoType = await response.json()
+    const resJson: UserInfoTypeWithFamiliarity = await response.json()
     setMatchedUsers(resJson)
   }
 
-  //ページ読み込み時にレンダリング
   useEffect(() => {
     fetchMatched().catch((err) => {
       console.error(err)
@@ -42,7 +46,15 @@ export const Matched = () => {
       <div>
         {Object.keys(matchedUsers).map((userId) => (
           <div key={userId} className={classes.container} onClick={() => transferToChat(userId)}>
-            <img src={matchedUsers[userId].photo} className={classes.photo}></img>
+            <img
+              src={matchedUsers[userId].photo}
+              className={classes.photo}
+              style={
+                isSecretMode
+                  ? { filter: `blur(${matchedUsers[userId].familiarity > 5 ? 0 : 10 - matchedUsers[userId].familiarity * 2}px)` }
+                  : null
+              }
+            ></img>
             <h3 className={classes.name}>{matchedUsers[userId].nickname}</h3>
             <p className={classes.message}>{matchedUsers[userId].selfIntro}</p>
           </div>
