48457804b1b53d22e6d040f88489b831ea437a10
takashi.hayashi <takashi.hayashi@woven-planet.global>
2022-09-29 09:45:08 +0900

    Navigate to /chat

--- src/components/matched/Matched.tsx
+++ src/components/matched/Matched.tsx
@@ -3,13 +3,19 @@ import { Header } from '@/components/header/Header'
 import { Footer } from '@/components/footer/Footer'
 import classes from '@/components/matched/style.css'
 import { UserInfoType } from '@/utils/types'
+import { useNavigate } from 'react-router-dom'
 
 //Recoil-on
 import { useRecoilValue } from 'recoil'
 import { userInfoState } from '@/atoms/userInfoAtom'
 
+type Props = {
+  partnerId: string
+}
+
 export const Matched = () => {
   const [matchedUsers, setMatchedUsers] = useState<UserInfoType>({})
+  const navigate = useNavigate()
 
   //RecoilでユーザIDを取得
   const userInfo = useRecoilValue(userInfoState)
@@ -36,7 +42,7 @@ export const Matched = () => {
       <Header menuExist={true} />
       <div>
         {Object.keys(matchedUsers).map((userId) => (
-          <div key={userId} className={classes.container}>
+          <div key={userId} className={classes.container} onClick={() => navigate('/chat', userId)}>
             <img src={matchedUsers[userId].photo} className={classes.photo}></img>
             <h3 className={classes.name}>{matchedUsers[userId].nickname}</h3>
             <p className={classes.message}>{matchedUsers[userId].selfIntro}</p>
