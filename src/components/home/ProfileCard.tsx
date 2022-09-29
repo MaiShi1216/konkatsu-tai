a2008a5cefede415f20ed7b7a2daa5e27e170a0a
masayuki <masayuki.inoue@woven-planet.global>
2022-09-29 17:03:23 +0900

    substitute card for vanilla css

--- src/components/home/ProfileCard.tsx
+++ src/components/home/ProfileCard.tsx
@@ -5,6 +5,13 @@ import { UserInfoContentType } from '@/utils/types'
 import { useRecoilValue } from 'recoil'
 import { userInfoState } from '@/atoms/userInfoAtom'
 
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
 type Familiarity = {
   familiarity?: number
 }
@@ -21,25 +28,33 @@ export const ProfileCard = (props: Props) => {
   const isSecretMode = userInfo[myId].isSecretMode
 
   return (
-    <React.Fragment>
-      <div className={classes.card}>
-        <div className={classes.image_frame}>
-          <img
-            src={props.user.photo}
-            alt=""
-            onClick={(event) => props.transferToProfile(event)}
-            id={props.id}
-            style={isSecretMode ? { filter: `blur(${props.user.familiarity > 5 ? 0 : 10 - props.user.familiarity * 2}px)` } : null}
-          />
-        </div>
-        <div className={classes.name}>{props.user.nickname}</div>
-        <div className={classes.like}>
-          <div>
-            <ThumbUpAltIcon style={{ fontSize: '1rem' }} />
-          </div>
-          <div className={classes.liked_num}>{props.user.likedNum > 99 ? '99+' : props.user.likedNum}</div>
-        </div>
-      </div>
-    </React.Fragment>
+    <Card sx={{ maxWidth: 345 }} onClick={(event) => props.transferToProfile(event)}>
+      <CardMedia component="img" image={props.user.photo} className={classes.card_media} />
+      <CardContent>
+        <Typography gutterBottom variant="h6" component="div">
+          {props.user.nickname}
+        </Typography>
+      </CardContent>
+    </Card>
+    // <React.Fragment>
+    //   <div className={classes.card}>
+    //     <div className={classes.image_frame}>
+    //       <img
+    //         src={props.user.photo}
+    //         alt=""
+    //         onClick={(event) => props.transferToProfile(event)}
+    //         id={props.id}
+    //         style={isSecretMode ? { filter: `blur(${props.user.familiarity > 5 ? 0 : 10 - props.user.familiarity * 2}px)` } : null}
+    //       />
+    //     </div>
+    //     <div className={classes.name}>{props.user.nickname}</div>
+    //     <div className={classes.like}>
+    //       <div>
+    //         <ThumbUpAltIcon style={{ fontSize: '1rem' }} />
+    //       </div>
+    //       <div className={classes.liked_num}>{props.user.likedNum > 99 ? '99+' : props.user.likedNum}</div>
+    //     </div>
+    //   </div>
+    // </React.Fragment>
   )
 }
