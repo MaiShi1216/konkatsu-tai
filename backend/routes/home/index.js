9ff54f2ae3d2717c544bc4db0d14d2e8ae0d2f3b
masayuki654 <111120750+masayuki654@users.noreply.github.com>
2022-09-29 09:59:29 +0900

    Hide like user (#36)
    * Co-authored-by: summerTakashi <summerTakashi@users.noreply.github.com>
    
    * Delete test.txt
    
    * Create home component
    
    * Create home main mock screen
    
    * Create ProfileCard component
    
    * Install material UI
    
    * Add get root at home page
    
    * Create frontend mock of homepage
    
    * Fetch all user info from database
    
    * Import userInfo type
    
    * Add profile root
    
    * Add profile component
    
    * Add profile element
    
    * Create CSS design with fixed user data
    
    * Implement transition from home to profile
    
    * Add likedNum count up function
    
    * Add likeButtonDisable function
    
    * Add useRecoilValue
    
    * Change from name to nickname
    
    * Apply camel case
    
    * Add reactions router
    
    * Add post method for like system
    
    * Delete profile root
    
    * Update backend/routes/home/index.js
    
    Co-authored-by: kan-mayoshi <111116632+kan-mayoshi@users.noreply.github.com>
    
    * Add get response body in error
    
    * Add likeUser id to likeHistory
    
    * Add hide like user function
    
    * Add timeout to transfer from profile to home
    
    Co-authored-by: kan-mayoshi <111116632+kan-mayoshi@users.noreply.github.com>

--- backend/routes/home/index.js
+++ backend/routes/home/index.js
@@ -1,14 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const router = require('express').Router()
 
 /* Import DB */
 const usersInfo = require('../../userInfo.json')
+const likeHistory = require('../../likeHistory.json')
 
 /* Successfully inquiry of authentication */
 router.get('/', (req, res) => {
-  console.log('GET /home')
+  const loginId = req.query.loginId
+  console.log(`GET /home?loginId=${loginId}`)
+
+  let body = {}
+  const likeHistoryArr = likeHistory[loginId]
+  Object.keys(usersInfo)
+    .filter((userId) => !likeHistoryArr.includes(userId))
+    .forEach((userId) => {
+      body[userId] = usersInfo[userId]
+    })
   res.status(200)
-  res.send(usersInfo)
+  res.send(body)
 })
 
 module.exports = router
