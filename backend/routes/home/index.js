d5751ebfede3c7f27673109f9ab423191917110d
masayuki <masayuki.inoue@woven-planet.global>
2022-09-29 13:19:48 +0900

    Delete unnecessary line in home index.js

--- backend/routes/home/index.js
+++ backend/routes/home/index.js
@@ -1,27 +1,15 @@
-// /* eslint-disable @typescript-eslint/no-unsafe-call */
-// <<<<<<< HEAD
-// /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-// /* eslint-disable @typescript-eslint/no-unsafe-argument */
-// =======
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 const router = require('express').Router()
 const likeHistory = require('../../likeHistory.json')
-
-/* Import DB */
-// <<<<<<< HEAD
-// const users = require('../../userInfo.json')
-// const chatHistory = require('../../chatHistory.json')
-
 const addFamiliarityToUsersInfo = require('../../utils/function')
 
 // /* Successfully inquiry of authentication */
 router.get('/', (req, res) => {
   const userId = req.query.userId
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   console.log(`GET /home?userId=${userId}`)
   let body = {}
   try {
@@ -38,23 +26,6 @@ router.get('/', (req, res) => {
     body = { message: 500 }
     res.status(500)
   }
-  // =======
-  // const usersInfo = require('../../userInfo.json')
-  // const likeHistory = require('../../likeHistory.json')
-
-  // /* Successfully inquiry of authentication */
-  // router.get('/', (req, res) => {
-  //   const loginId = req.query.loginId
-  //   console.log(`GET /home?loginId=${loginId}`)
-
-  //   let body = {}
-  //   const likeHistoryArr = likeHistory[loginId]
-  //   Object.keys(usersInfo)
-  //     .filter((userId) => !likeHistoryArr.includes(userId))
-  //     .forEach((userId) => {
-  //       body[userId] = usersInfo[userId]
-  //     })
-  //   res.status(200)
   res.send(body)
 })
 
