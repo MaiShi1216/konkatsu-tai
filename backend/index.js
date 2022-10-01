b9cbaa33b9ebd0b4237c5febd56f2e5841e0c256
kan-mayoshi <kan.mayoshi@woven-planet.global>
2022-10-01 11:11:32 +0900

    Add dynamic origin validation

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 // eslint-disable-next-line no-restricted-imports
 
 /* eslint-disable @typescript-eslint/no-var-requires */
@@ -7,25 +9,21 @@ const cors = require('cors')
 const port = 8081
 const router = require('./routes')
 
-const corsOptions = {
-  origin: 'http://localhost:8080',
-  credentials: true,
-  allowedHeaders: [
-    'Access-Control-Allow-Origin',
-    'Access-Control-Allow-Credentials',
-    'Origin',
-    'Authorization',
-    'Content-Type',
-    'Accept',
-    'x-api-key',
-    'x-user-agent',
-  ],
-}
-
-app.use(cors(corsOptions))
 app.use(express.json({ limit: '5mb' }))
 
 app.set('port', port)
+
+/* Dynamic origin Validation : Must define above of routing process */
+app.all('*', (req, res, next) => {
+  const allowedOriginList = ['http://localhost:8080', 'http://127.0.0.1:8080']
+  const origin = req.headers.origin
+  if (allowedOriginList.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use('/konkatsu-backend/api/v0', router)
 
 app.listen(app.get('port'), () => {
