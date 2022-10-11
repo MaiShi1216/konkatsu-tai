1e009e7d41928a2cb88cfea704c5457984f4275e
kan-mayoshi <111116632+kan-mayoshi@users.noreply.github.com>
2022-10-11 17:13:30 +0900

    Improve UI of Sign in (#62)
    * Add background
    
    * Delete unnecessary assets
    
    * Add dynamic origin validation
    
    * Add config to ignore eslint warning to .js
    
    Co-authored-by: kan-mayoshi <kan.mayoshi@woven-planet.global>

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,24 @@
-// eslint-disable-next-line no-restricted-imports
-
-/* eslint-disable @typescript-eslint/no-var-requires */
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = 8081
 const router = require('./routes')
 
-const corsOptions = {
-  origin: 'http://localhost:8080',
-  credentials: true,
-  allowedHeaders: [
-    'Access-Control-Allow-Origin',
-    'Access-Control-Allow-Credentials',
-    'Origin',
-    'Authorization',
-    'Content-Type',
-    'Accept',
-    'x-api-key',
-    'x-user-agent',
-  ],
-}
-
-app.use(cors(corsOptions))
 app.use(express.json({ limit: '5mb' }))
 
 app.set('port', port)
+
+/* Dynamic origin Validation : Must define above of routing process */
+app.all('*', (req, res, next) => {
+  const allowedOriginList = ['http://localhost:8080', 'http://127.0.0.1:8080']
+  const origin = req.headers.origin
+  if (allowedOriginList.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use('/konkatsu-backend/api/v0', router)
 
 app.listen(app.get('port'), () => {
